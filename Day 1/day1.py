d61218c30998f35f1a9b5cfee6d9f0189700768b
Ture Hassler <ture.hassler@gmail.com>
2023-12-01 10:41:01 +0100

    Day 1 part 1 done

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,6 +1,34 @@
 import numpy
 import math
+import re
 
-with open('day1.txt') as f:
+with open('Day 1/day1.txt') as f:
+    sum = 0
     for line in f:
         l = line.strip()
+
+        found_f = False
+        found_l = False
+
+        num1 = 0
+        num2 = 0
+        for char in l:
+            #print("In forward: " + char, " Num1: " + str(num1))
+            if char.isdigit():
+                num1 = int(char)
+                break
+
+        for char in reversed(l):
+            #print("In reverse: " + char, " Num2: " + str(num2))
+            if char.isdigit():
+                num2 = int(char)
+                break
+
+        sum += num1*10+num2
+
+    print(sum)
+
+
+
+    
+
