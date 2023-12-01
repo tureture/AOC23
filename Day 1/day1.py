a235c0777dece18b87385fef64dbed991bf92c48
Ture Hassler <ture.hassler@gmail.com>
2023-12-01 11:23:45 +0100

    Day 1 part 2 done

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -28,6 +28,42 @@ with open('Day 1/day1.txt') as f:
 
     print(sum)
 
+with open('Day 1/day1.txt') as f:
+
+    # regex expression to find the first substring that matches the values in the dict
+
+    words = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six":6, "seven":7, "eight":8, "nine":9, 
+             "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7": 7, "8":8, "9":9, "0":0}
+    
+
+    sum = 0
+    for line in f:
+        
+        min_index = len(line) +10
+        min_word = ""
+        max_index = -10
+        max_word = ""
+
+
+        for word in words:
+
+            if word in line:
+                index = line.find(word)
+
+                if index < min_index:
+                    min_index = index
+                    min_word = word
+
+                index = line.rfind(word)
+
+                if index > max_index:
+                    max_index = index
+                    max_word = word
+
+        sum += words[min_word]*10 + words[max_word]
+
+
+    print(sum)
 
 
     
